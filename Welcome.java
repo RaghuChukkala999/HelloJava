378d952d8e055fe1d6144da2c94b6238b96df79f
Raghu <raghuchukkala999@gmail.com>
2026-02-02 23:06:40 +0530

    commit

--- Welcome.java
+++ Welcome.java
@@ -1,14 +1,11 @@
 class Welcome{
 	public static void main(String[] args){
 		int a=10;
-<<<<<<< HEAD
-		int b=20;                int c=30;
-                int d=20;
-=======
 		int b=20;                
 		int c=30;
 		int d=40;
->>>>>>> c25c752298678b8a6b8683fb4be9b46cc8697a64
+
 		System.out.println("Welcome Java");
+     Yes , from note pad
 		}
 	}
