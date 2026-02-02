c25c752298678b8a6b8683fb4be9b46cc8697a64
RaghuChukkala999 <98801424+RaghuChukkala999@users.noreply.github.com>
2026-02-02 22:16:19 +0530

    checking merge conflects

--- Welcome.java
+++ Welcome.java
@@ -1,7 +1,9 @@
 class Welcome{
 	public static void main(String[] args){
 		int a=10;
-		int b=20;                int c=30;
+		int b=20;                
+		int c=30;
+		int d=40;
 		System.out.println("Welcome Java");
 		}
 	}
